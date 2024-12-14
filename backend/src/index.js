c34158ac3db034fa3ebdb1d83cf7466392e9772c
Patricio Avila <p.avil;af1998@gmail.com>
2024-12-14 14:32:40 -0300

    Handlig the routes and setting up the Socket.io to handle the messages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
 import  { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser";
 const app = express();
+
+
+import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 
 dotenv.config()
 const PORT = process.env.PORT
 app.use(express.json()) // Para parsear JSON en el cuerpo de las solicitudes
 
 app.use(cookieParser()); // Habilitar el middleware de cookies
-app.use("/api/auth", authRoutes);
 
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes)
 
 
 app.listen(PORT, () => {
---
+++ backend/src/lib/socket.js
@@ -0,0 +1,49 @@
+import { Server } from "socket.io";  // Socket.IO para manejo de conexiones en tiempo real
+import http from "http"; // Módulo HTTP de Node.js para crear el servidor
+import express from "express"; // Express para manejar rutas y solicitudes HTTP
+
+//crea la instancia apra express
+const app = express();
+
+// crea el servidor HTTP usando la isntancia express
+const server = http.createServer(app);
+
+// Inicia el servidor socket-IO i lo vincula al ervidor http
+const io = new Server(server, {
+    //configura cors apra un cliente especifico
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
+// Obtiene el coket id del usaurio conectado por el userId
+export function getReceiverSocketId(userId) {
+  return userSocketMap[userId]; // devuele el socketid correspondiente al userId
+}
+
+//mapa de usuarios coenctados
+const userSocketMap = {}; // {userId: socketId}
+
+// Configuraa los eventos para manejarlos 
+io.on("connection", (socket) => {
+  console.log("Usuario Conectados", socket.id); // muestra los usuarios conectados
+
+  //Obtiene al user por su id desde sus aprametris query.userId
+  const userId = socket.handshake.query.userId;
+  // si sus parametros son agregados,e ste se agrega al mapade usuarios coenctados
+  if (userId) userSocketMap[userId] = socket.id;
+
+  // Desde aqui los envie a la lista y actualiza los usuarios en linea 
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  // Manajando evento si un usuario se desconecta
+  socket.on("disconnect", () => {
+    console.log("Usuario Desconectado", socket.id);
+    // Elimina al usaurio de la lsita online
+    delete userSocketMap[userId];
+    // y aqui nuevamente actualiza la lista osea el mapa
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
+// exporta todas las instancias necesarias para usarlas en otras partes de la aplicación
+export { io, app, server };
