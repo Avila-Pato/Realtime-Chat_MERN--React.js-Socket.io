b64e94ff23ffd299902e6a0203f3d4a892827612
Patricio Avila <p.avil;af1998@gmail.com>
2024-12-18 14:37:28 -0300

    #HomePage - Utiliza el `useChatStore` para obtener el usuario seleccionado. - Muestra el componente `Sidebar` y dependiendo del estado de `selectedUser`, muestra `NoChatSelected` o `ChatContainer`. - Asegura una interfaz de usuario clara y accesible para los usuarios que desean iniciar o continuar un chat.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { useChatStore } from "../store/useChatStore";
 
-const HomePage= () => {
- return(
-    <div>Hola PAGE</div>
- )
-}
-export default HomePage
+import Sidebar from "../components/Sidebar";
+import NoChatSelected from "../components/NoChatSelected";
+import ChatContainer from "../components/ChatContainer";
+
+const HomePage = () => {
+  const { selectedUser } = useChatStore(); // Obtiene el usuario seleccionado del estado global.
+
+  return (
+    <div className="h-screen bg-base-200">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar /> {/* Muestra la barra lateral con los usuarios o conversaciones. */}
+
+            
+               {/* Condicional que muestra un componente u otro dependiendo si hay un chat seleccionado o no. */}
+            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomePage;
